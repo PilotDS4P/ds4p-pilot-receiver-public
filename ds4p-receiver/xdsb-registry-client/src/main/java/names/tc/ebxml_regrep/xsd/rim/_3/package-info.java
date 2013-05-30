0c9ff488f059b00bb3dc36067e508d1f759dc85b
Duane DeCouteau <duane.decouteau@gmail.com>
2013-05-30 12:34:17 -0600

    VA License Agreements

--- ds4p-receiver/xdsb-registry-client/src/main/java/names/tc/ebxml_regrep/xsd/rim/_3/package-info.java
+++ ds4p-receiver/xdsb-registry-client/src/main/java/names/tc/ebxml_regrep/xsd/rim/_3/package-info.java
@@ -1,2 +1,7 @@
+/**
+ * This software is being provided per FARS 52.227-14 Rights in Data - General.
+ * Any redistribution or request for copyright requires written consent by the
+ * Department of Veterans Affairs.
+ */
 @javax.xml.bind.annotation.XmlSchema(namespace = "urn:oasis:names:tc:ebxml-regrep:xsd:rim:3.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package oasis.names.tc.ebxml_regrep.xsd.rim._3;
--- ds4p-receiver/xdsb-registry-client/src/main/java/samhsa/schema/ds4p/xdsbregistry/message/package-info.java
+++ ds4p-receiver/xdsb-registry-client/src/main/java/samhsa/schema/ds4p/xdsbregistry/message/package-info.java
@@ -1,2 +1,7 @@
+/**
+ * This software is being provided per FARS 52.227-14 Rights in Data - General.
+ * Any redistribution or request for copyright requires written consent by the
+ * Department of Veterans Affairs.
+ */
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://schema.samhsa.gov/ds4p/XDSbRegistry/Message", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package gov.samhsa.schema.ds4p.xdsbregistry.message;
