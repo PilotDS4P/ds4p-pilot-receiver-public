270c6ed1676def8b2a71802cb614d0f08c519b91
Duane DeCouteau <duane.decouteau@gmail.com>
2013-05-30 12:38:52 -0600

    VA License Agreements

--- ds4p-receiver/xdsb-repository-client/src/test/java/gov/samhsa/ds4ppilot/ws/client/XdsRepositorybImpl.java
+++ ds4p-receiver/xdsb-repository-client/src/test/java/gov/samhsa/ds4ppilot/ws/client/XdsRepositorybImpl.java
@@ -1,4 +1,9 @@
 /**
+ * This software is being provided per FARS 52.227-14 Rights in Data - General.
+ * Any redistribution or request for copyright requires written consent by the
+ * Department of Veterans Affairs.
+ */
+/**
  * Please modify this class to meet your needs
  * This class is not complete
  */
